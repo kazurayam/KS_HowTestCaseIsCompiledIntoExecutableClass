fdd919a72dd970f628ec1c18364b5d69f9751b16
kazurayam <kazuaki.urayama@gmail.com>
2025-07-11 09:02:33 +0900

    first commit

---
+++ Object Repository/Page_Login/The Same Text.rs
@@ -0,0 +1,17 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>The Same Text</name>
+   <tag></tag>
+   <elementGuidId>7872c0a1-959e-470c-a409-6e7f4454ed9e</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>id</name>
+      <type>Main</type>
+      <value>btn-login</value>
+      <webElementGuid>4d76e085-e3a8-4e80-98fb-6684d6862579</webElementGuid>
+   </webElementProperties>
+</WebElementEntity>
---
+++ Test Suites/junit4/junittutorial/All.ts
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>All</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>944f3971-3078-423d-9344-10252eebaa95</testSuiteGuid>
+   <testCaseLink>
+      <guid>6cd374ff-65d9-4e40-865d-95d253c6cb5f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/juni4/junittutorial/CalculatorFailingTestRunner</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>f9253304-f13b-4ab2-8454-13b58d228c1c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/juni4/junittutorial/CalculatorTestRunner</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
